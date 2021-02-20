1f8a67c306ec60bfee476fbf5b7d946d125f93ca
Beandip <brando.lugo@gmail.com>
2021-02-20 15:16:15 -0600

    Refactored and addded some destructouring of the list

--- src/App.js
+++ src/App.js
@@ -1,58 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
 const App = () => {
   const stories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
-    }
+    },
   ];
 
-  //Do stuff
-  const handleChage = event => {
-    console.log(event.target.value)
+  //Lift state
+  const [searchTerm, setSearchTerm] = React.useState("...");
+
+  //Event for the callback handler
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <h1>Beandips React App</h1>
+      <h1>My React Hacks</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChage}/>
-
-      <hr/>
-      
-      <List list={stories}/>
+      <Search search={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
     </div>
   );
 };
 
-const List = props =>
-  props.list.map(item => (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-  ));
+const List = ({ list }) =>
+  list.map((item) => <Item key={item.objectID} item={item} />);
 
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
+
+const Search = ({ search, onSearch }) => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={onSearch} />
+      <p>
+        Searching for <strong>{search}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default App;
